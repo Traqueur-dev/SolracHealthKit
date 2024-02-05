d59ea07a54c2ce5e7a0c33a7ca36dad6831f2cea
Traqueur-dev <traqueur.dev@gmail.com>
2024-02-05 14:33:10 +0100

    add healthkit to solrac

---
+++ src/main/java/fr/traqueur/solrac/healthkit/HealthKitManager.java
@@ -0,0 +1,72 @@
+package fr.traqueur.solrac.healthkit;
+
+import fr.traqueur.solrac.healthkit.exceptions.HealthKitNoExistException;
+import fr.traqueur.solrac.healthkit.exceptions.HealthKitWrongMaterialException;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HealthKitManager {
+
+    private final JavaPlugin plugin;
+
+    private Material HEALTHKIT_MATERIAL;
+    private int HEALTHKIT_CUSTOMMODELDATA;
+    private double HEALTH_TO_GIVE;
+
+    public HealthKitManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void init() throws HealthKitNoExistException, HealthKitWrongMaterialException {
+        FileConfiguration config = plugin.getConfig();
+        if(!config.contains("healthkit") || !config.contains("healthkit.material")
+                || !config.contains("healthkit.custommodeldata") || !config.contains("healthkit.health")) {
+            throw new HealthKitNoExistException();
+        }
+        String materialName = config.getString("healthkit.material");
+        int data = config.getInt("healthkit.custommodeldata");
+        Material material = Material.getMaterial(materialName.toUpperCase());
+        if(material == null) {
+            throw new HealthKitWrongMaterialException();
+        }
+        HEALTHKIT_MATERIAL = material;
+        HEALTHKIT_CUSTOMMODELDATA = data;
+        HEALTH_TO_GIVE = config.getDouble("healthkit.health");
+    }
+
+    public void handleInteract(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        Player player = event.getPlayer();
+        ItemStack item = event.getItem();
+
+        if(!action.isRightClick()) {
+            return;
+        }
+
+        if(!this.isHealthKit(item)) {
+            return;
+        }
+
+        event.setCancelled(true);
+        item.setAmount(0);
+        player.setHealth(player.getHealth() + HEALTH_TO_GIVE*2);
+    }
+
+    private boolean isHealthKit(ItemStack item) {
+        if(item == null || item.isEmpty() || !item.hasItemMeta()) {
+            return false;
+        }
+
+        ItemMeta healthMeta = item.getItemMeta();
+
+        return (item.getType() == HEALTHKIT_MATERIAL) &&
+                healthMeta.hasCustomModelData() &&
+                (healthMeta.getCustomModelData() == HEALTHKIT_CUSTOMMODELDATA);
+    }
+}
